e0faed68455fbe7883ea64d0badc0df66b473123
Ann-Martines <31079869+Ann-Martines@users.noreply.github.com>
2017-12-19 22:48:54 +0200

    Add files via upload

---
+++ TranslatorInLEET/WPFЗаданиеLeet/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFЗаданиеLeet
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// By pressing there is a transfer
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btn_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				this.TxtTranslate.Clear();
+				Translate tr = new Translate();
+				if (this.RbToLeet.IsChecked == true)
+				{
+					foreach (var i in this.Txt.Text.ToUpper())
+					{
+						tr.ToLeet(i.ToString());
+						if (tr.Result != null)
+							this.TxtTranslate.Text += tr.Result;
+						else
+							this.TxtTranslate.Text += i.ToString().ToLower();
+					}
+				}
+				if(this.RbToEnglish.IsChecked == true)
+				{
+					foreach(var i in this.Txt.Text.ToUpper())
+					{
+						tr.ToEnglish(i.ToString());
+						if (tr.Result != null)
+							this.TxtTranslate.Text += tr.Result.ToLower();
+						else
+							this.TxtTranslate.Text += i.ToString().ToLower();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
---
+++ TranslatorInLEET/WPFЗаданиеLeet/Translate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFЗаданиеLeet
+{
+	class Translate
+	{
+		Dictionary<string, string> dicToLeet = null;
+		Dictionary<string, string> dicToEnglish = null;
+		string result = null;
+
+		/// <summary>
+		/// The property returning the translated symbol
+		/// </summary>
+		public string Result
+		{
+			get
+			{
+				return this.result;
+			}
+		}
+
+		/// <summary>
+		/// Filling of the dictionary with keys and values
+		/// </summary>
+		public Translate()
+		{
+			dicToLeet = new Dictionary<string, string>();
+			dicToLeet.Add("A", "4");
+			dicToLeet.Add("B", "8");
+			dicToLeet.Add("E", "3");
+			dicToLeet.Add("G", "6");
+			dicToLeet.Add("I", "1");
+			dicToLeet.Add("O", "0");
+			dicToLeet.Add("S", "5");
+			dicToLeet.Add("T", "7");
+			dicToLeet.Add("Z", "2");
+
+			dicToEnglish = new Dictionary<string, string>();
+			dicToEnglish.Add("4", "A");
+			dicToEnglish.Add("8", "B");
+			dicToEnglish.Add("3", "E");
+			dicToEnglish.Add("6", "G");
+			dicToEnglish.Add("1", "I");
+			dicToEnglish.Add("0", "O");
+			dicToEnglish.Add("5", "S");
+			dicToEnglish.Add("7", "T");
+			dicToEnglish.Add("2", "Z");
+
+
+		}
+
+		/// <summary>
+		/// Makes a transfer to leet letters
+		/// </summary>
+		/// <param name="str"></param>
+		public void ToLeet(string str)
+		{
+			result = null;
+			this.dicToLeet.TryGetValue(str, out this.result);
+		}
+
+		/// <summary>
+		/// Makes a transfer to the English letters
+		/// </summary>
+		/// <param name="str"></param>
+		public void ToEnglish(string str)
+		{
+
+			this.result = null;
+			this.dicToEnglish.TryGetValue(str, out this.result);
+		}
+	}
+}
